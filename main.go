39e7f0efb2fb4f15715bfaf1c31bc64cc92d33d1
Nick Foden <nickfoden@gmail.com>
2022-02-04 19:56:15 -0500

    Section 2

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	card := newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
