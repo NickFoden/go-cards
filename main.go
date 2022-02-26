40d33c2a658d955bbdb03cefe66ce0f38f586e63
Nick Foden <nickfoden@gmail.com>
2022-02-26 11:00:59 -0500

    new deck from file

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 func main() {
-	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	cards := newDeckFromFile("my_cards")
+	cards.print()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards.txt")
 }
